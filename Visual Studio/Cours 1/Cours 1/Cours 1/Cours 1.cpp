f5e810c6093298b17b20679e6cef47f3311779a6
oct0punk <55735176+CouetteAllan@users.noreply.github.com>
2021-10-14 17:33:01 +0200

    explicit

--- Visual Studio/Cours 1/Cours 1/Cours 1/Cours 1.cpp
+++ Visual Studio/Cours 1/Cours 1/Cours 1/Cours 1.cpp
@@ -28,7 +28,7 @@ void Memcpy(char* dst, const char* src, int len) {
 		dst[i] = src[i];
 }
 
-const char * StrChr(const char * grange, char chat) {
+const char* StrChr(const char* grange, char chat) {
 	int index = 0;
 	char found;
 	while (*grange) {
@@ -40,23 +40,23 @@ const char * StrChr(const char * grange, char chat) {
 	return nullptr;
 }
 
-const char * StrStr(const char * a, const char* b) {
-	
+const char* StrStr(const char* a, const char* b) {
+
 	size_t bLen = strlen(b);
 
-	while (*a){
+	while (*a) {
 		bool found = true;
 
-		for (size_t i = 0; i < bLen; i++){
+		for (size_t i = 0; i < bLen; i++) {
 			if (a[i] != b[i]) {
 				a++;
 				found = false;
 				break;
 			}
 		}
-		if (found) 
+		if (found)
 			return a;
-		
+
 
 	}
 	return nullptr;
@@ -100,7 +100,7 @@ int Mul(int a, int b) {
 
 	if (b == 0 || a == 0)
 		return 0;
-	
+
 	if (b == 1)
 		return a;
 	if (a == 1)
@@ -110,6 +110,20 @@ int Mul(int a, int b) {
 		return a + Mul(a, b - 1);
 	else
 		return -a + Mul(a, b + 1);
+}
+
+int MulExplicit(int a, int b, int tmpResult) {
+
+	if (b == 0 || a == 0)
+		return 0;
+
+	if (b == 1)
+		return a + tmpResult;
+	if (a == 1)
+		return b;
+
+	if (b > 0)
+		return MulExplicit(a, b - 1, a + tmpResult);
 }
 
 int Div(int a, int b) {
@@ -124,6 +138,20 @@ int Div(int a, int b) {
 		return 0;
 
 	return 1 + Div(Sub(a, b), b);
+}
+
+int DivExplicit(int a, int b,int tmpResult) {
+	if (b == 0) {
+		throw "division by zero";
+	}
+	if (a == 0)
+		return tmpResult;
+	if (b == 1)
+		return a + tmpResult;
+	if (a < b)
+		return 0;
+
+	return DivExplicit(Sub(a, b), b, 1 + tmpResult);
 }
 
 int Mod(int a, int b) {
@@ -143,20 +171,11 @@ int Mod(int a, int b) {
 }
 
 int Mod2(int a, int b) {
-	return a - Mul(Div(a, b) ,b);
+	return a - Mul(Div(a, b), b);
 
 }
 
-int Sqr(int a) {
-	return Mul(a, a);
-}
-
-
-int Len(int vecX, int vecY) {
-	return 0;
-}
-
-
+#pragma region StrRecurrence
 int Strlen(const char* chaine) {
 	int idx = 0;
 	while (chaine[idx] != 0) {
@@ -165,7 +184,6 @@ int Strlen(const char* chaine) {
 
 	return idx;
 }
-int indx = 0;
 int StrlenRecursive(const char* chaine) {
 	if (nullptr == chaine) {
 		return 0;
@@ -176,7 +194,7 @@ int StrlenRecursive(const char* chaine) {
 	}
 
 	return 1 + StrlenRecursive(chaine + 1);
-	
+
 }
 
 
@@ -218,7 +236,7 @@ void StrncpyRecursive(const char* source, char* destination, int nbChars) {
 	if (nbChars == 0)
 		return;
 
-	StrncpyRecursive(source + 1, destination + 1,nbChars);
+	StrncpyRecursive(source + 1, destination + 1, nbChars);
 
 }
 
@@ -291,7 +309,7 @@ const char* StrStrRec(const char* str, char* sub) {
 	if (!sub)
 		return nullptr;
 
-	if (IsPrefixe(str,sub))
+	if (IsPrefixe(str, sub))
 		return str;
 	else
 		return StrStrRec(str + 1, sub);
@@ -308,10 +326,10 @@ char* findEnd(char* str) {
 	}
 }
 
-char* Strcat(const char* source,char* destination) {
+char* Strcat(const char* source, char* destination) {
 	if (nullptr == destination)
 		return nullptr;
-	
+
 	char* dstEnd = findEnd(destination);
 	StrcpyRecursive(source, dstEnd);
 	return destination;
@@ -328,6 +346,10 @@ int Strcmp(const char* a, const char* b) {
 	return Strcmp(a + 1, b + 1);
 }
 
+#pragma endregion
+
+
+
 int main() {
 	int c = Mod2(26, 3);
 
@@ -335,13 +357,15 @@ int main() {
 	const char* toto = "Bravo le renard blanc";
 	char tota[] = "renard";
 
-	
+
 	//char* tata = Strcat(toto, tota);
 	//tata = Strcat(tata, tota);
 
 	bool cas0 = Strcmp("sapin", "sapin") == 0;
 	bool casMinus1 = Strcmp("sapin", "Grandsapin") == -1;
 	bool cas1 = Strcmp("Grandsapin", "sapin") == 1;
+
+	int e = DivExplicit(15, 3, 0);
 
 
 	return 0;
