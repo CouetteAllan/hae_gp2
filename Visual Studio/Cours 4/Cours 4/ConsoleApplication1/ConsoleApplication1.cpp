32d1f5a4b0471cd7b72e41bc4ea0474c7904d60b
oct0punk <55735176+CouetteAllan@users.noreply.github.com>
2021-11-04 17:28:48 +0100

    sfml tank

--- Visual Studio/Cours 4/Cours 4/ConsoleApplication1/ConsoleApplication1.cpp
+++ Visual Studio/Cours 4/Cours 4/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Main.hpp>
+#include <math.h>
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
@@ -8,9 +9,10 @@ int main()
     float width = size.x;
     float height = size.y;
 
+    sf::Vector2f localMousePosition = (sf::Vector2f)sf::Mouse::getPosition(window);
 
 
-    sf::RectangleShape shape(sf::Vector2f(40,80));
+    sf::RectangleShape shape(sf::Vector2f(80,40));
     sf::Vector2f center;
     center.x = shape.getSize().x / 2;
     center.y = shape.getSize().y / 2;
@@ -18,11 +20,34 @@ int main()
     shape.setFillColor(sf::Color::Blue);
     shape.setOutlineThickness(10);
     shape.setOutlineColor(sf::Color::Magenta);
-    shape.setPosition(width/2, height/2);
+    shape.setPosition(width/2, height/2 + 50);
+    
+    
+    sf::RectangleShape gun(sf::Vector2f(10,90));
+    sf::Vector2f gunCenter;
+    gunCenter.x = gun.getSize().x / 2;
+    gunCenter.y = gun.getSize().y / 2;
+    gun.setOrigin(sf::Vector2f(5,75));
+    gun.setFillColor(sf::Color::Red);
+    gun.setOutlineThickness(2);
+    gun.setOutlineColor(sf::Color::Cyan);
+    gun.setPosition(shape.getPosition().y, shape.getPosition().x);
+
+    sf::RectangleShape particles(sf::Vector2f(10, 10));
+    particles.setPosition(gunCenter.x,gunCenter.y - 75);
+    particles.setFillColor(sf::Color::White);
+    bool shoot = false;
+
+    sf::CircleShape mouseCircle(10);
+    mouseCircle.setOrigin(10,10);
+    mouseCircle.setFillColor(sf::Color::Yellow);
+    mouseCircle.setPosition(sf::Mouse::getPosition(window).x,sf::Mouse::getPosition(window).y);
     
     sf::Mouse::setPosition(sf::Vector2i(width / 2, height / 2),window);
     while (window.isOpen())
     {
+
+        localMousePosition = (sf::Vector2f)sf::Mouse::getPosition(window);
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -47,28 +72,42 @@ int main()
         }
 
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
             shape.move(-3.f, 0.f);
         
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
             shape.move(3.f, 0.f);
         
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
             shape.move(0.f, -3.f);
         
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
             shape.move(0.f, 3.f);
-        
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-            sf::Vector2f localPosition = (sf::Vector2f)sf::Mouse::getPosition(window);
-            sf::RectangleShape particles(localPosition);
-            window.draw(particles);
-        }
-            
 
+
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            shoot = true;
+        }
+        else
+            shoot = false;
+        
+
+        auto angle = atan2((localMousePosition.x - gun.getPosition().x), -(localMousePosition.y - gun.getPosition().y));
+        angle = (angle / 3.1415926) * 180;
+        gun.setRotation(angle);
+        gun.setPosition(shape.getPosition().x, shape.getPosition().y);
+        particles.setPosition(localMousePosition);
+
+
+        mouseCircle.setPosition(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
 
         window.clear();
         window.draw(shape);
+        window.draw(gun);
+        window.draw(mouseCircle);
+        if(shoot)
+            window.draw(particles);
         window.display();
     }
 
